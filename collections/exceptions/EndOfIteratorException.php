0fbf7e445a600d4575b1a1dc8bdd263aa09b9d2f
tesarek <tesarek@cybergenics.cz>
2012-10-23 17:01:02 +0200

    EndOfIterator exception

---
+++ collections/exceptions/EndOfIteratorException.php
@@ -0,0 +1,6 @@
+<?php
+namespace tjsd\collections\exceptions;
+
+class EndOfIteratorException extends \RuntimeException {
+    
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 define('DOCUMENT_ROOT', __DIR__ . '/..');
+
 include DOCUMENT_ROOT . '/collections/exceptions/OffsetNotFoundException.php';
+include DOCUMENT_ROOT . '/collections/exceptions/EndOfIteratorException.php';
 
 include DOCUMENT_ROOT . '/collections/Collection.php';
 include DOCUMENT_ROOT . '/collections/ArrayObject.php';
