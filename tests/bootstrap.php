ff60d5f1da11038330982fa4d2e12600341084ac
tesarek <tesarek@cybergenics.cz>
2012-10-23 15:13:14 +0200

    Full path to class files changed to realative

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
-define('DOCUMENT_ROOT', 'P:\datastructures\collections');
-include DOCUMENT_ROOT . './exceptions/OffsetNotFoundException.php';
+define('DOCUMENT_ROOT', __DIR__ . '/..');
+include DOCUMENT_ROOT . '/collections/exceptions/OffsetNotFoundException.php';
 
-include DOCUMENT_ROOT . './Collection.php';
-include DOCUMENT_ROOT . './ArrayObject.php';
-include DOCUMENT_ROOT . './Iterator.php';
-include DOCUMENT_ROOT . './ArrayIterator.php';
+include DOCUMENT_ROOT . '/collections/Collection.php';
+include DOCUMENT_ROOT . '/collections/ArrayObject.php';
+include DOCUMENT_ROOT . '/collections/Iterator.php';
+include DOCUMENT_ROOT . '/collections/ArrayIterator.php';
