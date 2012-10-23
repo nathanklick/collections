a9fae9288fccbb9c7ac7722e7579e27d63b6ff18
tesarek <tesarek@cybergenics.cz>
2012-10-23 14:46:52 +0200

    ArrayObject tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+define('DOCUMENT_ROOT', 'P:\datastructures\collections');
+include DOCUMENT_ROOT . './exceptions/OffsetNotFoundException.php';
+
+include DOCUMENT_ROOT . './Collection.php';
+include DOCUMENT_ROOT . './ArrayObject.php';
+include DOCUMENT_ROOT . './Iterator.php';
+include DOCUMENT_ROOT . './ArrayIterator.php';
---
+++ tests/collections/ArrayObjectTest.php
@@ -0,0 +1,91 @@
+<?php
+namespace tjsd\collections;
+
+class ArrayObjectTest extends \PHPUnit_Framework_TestCase {
+    protected $arrayObject;
+
+    protected function setUp() {
+        $this->arrayObject = new ArrayObject();
+    }
+
+    public function testCountOnEmpty() {
+        $this->assertEquals(0, $this->arrayObject->count());
+    }
+    
+    public function testCountOnNotEmpty() {
+        $this->arrayObject->offsetSet('existingOffset', 'foo');
+        $this->assertEquals(1, $this->arrayObject->count());
+    }
+
+    public function testGetIterator() {
+        $this->assertInstanceOf('\tjsd\collections\Iterator', $this->arrayObject->getIterator());
+    }
+
+    public function testExistingOffsetExists() {
+        $this->arrayObject->offsetSet('existingOffset', 'foo');
+        $this->assertTrue($this->arrayObject->offsetExists('existingOffset'));
+    }
+    
+    public function testNonExistingOffsetExists() {
+        $this->assertFalse($this->arrayObject->offsetExists('nonExistingOffset'));
+    }
+
+    public function testOffsetSetAndGet() {
+        $this->arrayObject->offsetSet('existingOffset', 'foo');
+        $this->assertEquals('foo', $this->arrayObject->offsetGet('existingOffset'));
+    }
+
+    /**
+     * @expectedException \tjsd\collections\exceptions\OffsetNotFoundException
+     */
+    public function testNonExistingOffsetGet() {
+        $this->arrayObject->offsetGet('nonExistingOffset');
+    }
+    
+    /**
+     * @expectedException \tjsd\collections\exceptions\OffsetNotFoundException
+     */
+    public function testOffsetUnsetAndGet() {
+        $this->arrayObject->offsetSet('existingOffset', 'foo');
+        $this->arrayObject->offsetUnset('existingOffset');
+        $this->arrayObject->offsetGet('existingOffset');
+    }
+
+    public function test__toString() {
+        $expected = 'a:3:{s:3:"foo";s:3:"bar";i:1;b:1;i:2;a:1:{i:0;a:1:{i:0;b:0;}}}';
+        
+        $this->arrayObject['foo'] = 'bar';
+        $this->arrayObject[1] = TRUE;
+        $this->arrayObject[] = array(array(FALSE));
+        
+        $this->assertEquals($expected, $this->arrayObject->__toString());
+    }
+
+    public function testToArray() {
+        $expected = array();
+        $expected['foo'] = 'bar';
+        $expected[1] = TRUE;
+        $expected[] = array(array(FALSE));
+        
+        $this->arrayObject['foo'] = 'bar';
+        $this->arrayObject[1] = TRUE;
+        $this->arrayObject[] = array(array(FALSE));
+        
+        $this->assertEquals($expected, $this->arrayObject->toArray());
+    }
+
+    public function testIsEmptyOnEmpty() {
+        $this->assertTrue($this->arrayObject->isEmpty());
+    }
+    
+    public function testIsEmptyOnNonEmpty() {
+        $this->arrayObject->offsetSet('existingOffset', 'foo');
+        $this->assertFalse($this->arrayObject->isEmpty());
+    }
+
+    public function testClear() {
+        $this->arrayObject->offsetSet('existingOffset', 'foo');
+        $this->arrayObject->clear();
+        $this->assertTrue($this->arrayObject->isEmpty());
+    }
+}
