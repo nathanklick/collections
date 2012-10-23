b0554d0be39393ae12b6b49d885c10f9373c09fa
tesarek <tesarek@cybergenics.cz>
2012-10-23 14:45:37 +0200

    Skelleton for ArrayIterator

---
+++ collections/ArrayIterator.php
@@ -0,0 +1,29 @@
+<?php
+namespace tjsd\collections;
+
+class ArrayIterator implements Iterator {
+    
+    public function __construct(array $data) {
+        
+    }
+    
+    public function current() {
+        
+    }
+
+    public function key() {
+        
+    }
+
+    public function next() {
+        
+    }
+
+    public function rewind() {
+        
+    }
+
+    public function valid() {
+        
+    }
+}
