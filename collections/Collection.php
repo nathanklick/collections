98faf07934513b77e8c99dbc81fc428bc1d1a95b
tesarek <tesarek@cybergenics.cz>
2012-10-23 14:44:28 +0200

    Collection interface

---
+++ collections/Collection.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Jakub Tesárek <programmer@jakubtesarek.cz> (http://jakubtesarek.cz)
+ * @copyright Copyright (c) 2012, Jakub Tesárek
+ */
+
+namespace tjsd\collections;
+
+/**
+ * General purpose collection interface.
+ */
+interface Collection extends \Countable, \IteratorAggregate {
+    
+    /**
+     * Returns string representation of all elements in collection in serialize format.
+     * 
+     * @returns string string represenation of collection
+     */
+    public function __toString();
+    
+    /**
+     * @return array all elements as an array
+     */
+    public function toArray();
+    
+    /**
+     * @return boolean TRUE if collection contains no elements
+     */
+    public function isEmpty();
+    
+    /**
+     * Empty collection (remove all elements)
+     * 
+     * @return NULL
+     */
+    public function clear();
+}
