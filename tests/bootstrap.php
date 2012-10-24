4231f0c5002932fb4419076f907043b5dc9528e6
tesarek <tesarek@cybergenics.cz>
2012-10-24 21:49:16 +0200

    moving whole project into "tjsd" directory.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,12 @@
 <?php
+// require class loader
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require_once __DIR__ . '/../vendor/autoload.php';
+
+
+/**
 define('DOCUMENT_ROOT', __DIR__ . '/..');
+//include DOCUMENT_ROOT . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 include DOCUMENT_ROOT . '/collections/exceptions/OffsetNotFoundException.php';
 include DOCUMENT_ROOT . '/collections/exceptions/EndOfIteratorException.php';
@@ -10,3 +17,4 @@ include DOCUMENT_ROOT . '/collections/Iterator.php';
 include DOCUMENT_ROOT . '/collections/ArrayIterator.php';
 include DOCUMENT_ROOT . '/collections/Stack.php';
 include DOCUMENT_ROOT . '/collections/ArrayStack.php';
+*/
---
+++ tjsd/collections/ArrayQueue.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @author Jakub Tesárek <programmer@jakubtesarek.cz> (http://jakubtesarek.cz)
+ * @copyright Copyright (c) 2012, Jakub Tesárek
+ */
+
+namespace tjsd\collections;
+
+class ArrayQueue implements Queue {
+    private $data;
+    
+    public function __construct(array $initialData = array()) {
+        $this->data = $initialData;
+    }
+    
+    public function push($element) {
+        array_unshift($this->data, $element);
+    }
+    
+    public function poll() {
+        array_shift($this->data);
+    }
+    
+    public function peek() {
+        return reset($this->data);
+    }
+
+    public function __toString() {
+        return serialize($this->toArray());
+    }
+
+    public function clear() {
+        $this->data = array();
+    }
+
+    public function count() {
+        return count($this->data);
+    }
+
+    public function getIterator() {
+        return new ArrayIterator($this->toArray());
+    }
+
+    public function isEmpty() {
+        return $this->count() === 0;
+    }
+
+    public function toArray() {
+        /**
+         * Array keys chnges during live cycle of queue. Using array_values() to
+         * make sure that array will be indexed starting 0.
+         */
+        return array_values($this->data);
+    }
+}
