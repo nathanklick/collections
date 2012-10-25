ae258f54de705f7202ca0cbeb5c8e84794c689f2
tesarek <tesarek@cybergenics.cz>
2012-10-25 23:58:38 +0200

    copyright mention to test files

--- tests/tjsd/collections/ArrayObjectTest.php
+++ tests/tjsd/collections/ArrayObjectTest.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author Jakub Tesárek <programmer@jakubtesarek.cz> (http://jakubtesarek.cz)
+ * @copyright Copyright (c) 2012, Jakub Tesárek
+ */
+
 namespace tjsd\collections;
 
 class ArrayObjectTest extends \PHPUnit_Framework_TestCase {
--- tests/tjsd/collections/ArrayStackTest.php
+++ tests/tjsd/collections/ArrayStackTest.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @author Jakub Tesárek <programmer@jakubtesarek.cz> (http://jakubtesarek.cz)
+ * @copyright Copyright (c) 2012, Jakub Tesárek
+ */
 
 namespace tjsd\collections;
 
