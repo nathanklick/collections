fc899eea0512f0e8b9ba366d2fd55825d1cf599e
tesarek <tesarek@cybergenics.cz>
2012-12-13 15:20:45 +0100

    DuplicateEntry exception

---
+++ tjsd/collections/exceptions/DuplicateEntryException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Jakub Tesárek <programmer@jakubtesarek.cz> (http://jakubtesarek.cz)
+ * @copyright Copyright (c) 2012, Jakub Tesárek
+ */
+
+namespace tjsd\collections\exceptions;
+
+class DuplicateEntryException extends \UnexpectedValueException implements CollectionException {
+    
+}
